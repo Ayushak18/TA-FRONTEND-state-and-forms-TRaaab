1a82512d25b1082cbd7c97e86f069bd7990e87e1
Ayushak18 <aayushkamboj7@gmail.com>
2021-07-11 03:02:25 +0530

    Max Step

---
+++ block-BRaabf/counter-one/src/App.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+      step: 1,
+      active: '',
+      maxValue: Infinity,
+    };
+  }
+
+  handleIncrement = () => {
+    if (this.state.maxValue - this.state.counter >= this.state.step) {
+      this.setState({
+        counter: this.state.counter + this.state.step,
+      });
+    }
+  };
+
+  handleDecrement = () => {
+    this.setState({
+      counter: this.state.counter - this.state.step,
+    });
+  };
+
+  handleReset = () => {
+    this.setState({
+      counter: 0,
+      step: 1,
+      maxValue: Infinity,
+    });
+  };
+
+  setStep = (event) => {
+    let step = Number(event.target.innerText);
+    // event.target.className = 'active';
+    this.setState({
+      step: step,
+      // active: 'active',
+    });
+  };
+
+  setMax = (event) => {
+    let maxValue = Number(event.target.innerText);
+    this.setState({
+      maxValue: maxValue,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <h1>{this.state.counter}</h1>
+        </div>
+        <div className="steps">
+          <h2>Steps</h2>
+          <div>
+            <button onClick={(event) => this.setStep(event)}>5</button>
+            <button onClick={(event) => this.setStep(event)}>10</button>
+            <button onClick={(event) => this.setStep(event)}>15</button>
+          </div>
+        </div>
+        <div>
+          <h2>Max Value</h2>
+          <button onClick={(event) => this.setMax(event)}>15</button>
+          <button onClick={(event) => this.setMax(event)}>100</button>
+          <button onClick={(event) => this.setMax(event)}>200</button>
+        </div>
+        <div>
+          <button onClick={this.handleIncrement}>Increament</button>
+          <button onClick={this.handleDecrement}>Decreament</button>
+          <button onClick={this.handleReset}>Reset</button>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
